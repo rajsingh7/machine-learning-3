9a9cb11224819d6b5b29f94cf32ca0bec4592a7b
wilsonfreitas <wilson.freitas@gmail.com>
2018-09-11 04:48:34 -0300

    Changed visuals.py to display graphics correctly
    Function evaluate shows graphics with incorrect sizes and dimensions.

--- projects/finding_donors/visuals.py
+++ projects/finding_donors/visuals.py
@@ -110,8 +110,11 @@ def evaluate(results, accuracy, f1):
     patches = []
     for i, learner in enumerate(results.keys()):
         patches.append(mpatches.Patch(color = colors[i], label = learner))
-    pl.legend(handles = patches, bbox_to_anchor = (-.80, 2.53), \
+    # pl.legend(handles = patches, bbox_to_anchor = (-.80, 2.53), \
+    #            loc = 'upper center', borderaxespad = 0., ncol = 3, fontsize = 'x-large')
+    fig.legend(handles = patches, bbox_to_anchor = (0.5, 1.05), \
                loc = 'upper center', borderaxespad = 0., ncol = 3, fontsize = 'x-large')
+
     
     # Aesthetics
     pl.suptitle("Performance Metrics for Three Supervised Learning Models", fontsize = 16, y = 1.10)
